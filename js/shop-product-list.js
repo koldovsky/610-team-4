53f8598bd467dc527e2fe97c5aed798f2228dbde
lilyalya <shevtsova.liliia@gmail.com>
2021-07-26 13:27:50 +0000

    Added all products and css to the product list

--- js/shop-product-list.js
+++ js/shop-product-list.js
@@ -22,6 +22,96 @@ const productListJson = `[
         "title": "Adidas Compression Tee",
         "imageUrl": "img/shop/adidas-compression-tee.jpg",
         "price": 33
+    },
+    {
+        "id": "5",
+        "title": "Asics Jordan 2000",
+        "imageUrl": "img/shop/asics-jordan-2000.jpg",
+        "price": 200
+    },
+    {
+        "id": "6",
+        "title": "Nike Amanda Running Shoes",
+        "imageUrl": "img/shop/nike-amanda-running-shoes.jpg",
+        "price": 120
+    },
+    {
+        "id": "7",
+        "title": "Nike Classic Polo T-Shirt",
+        "imageUrl": "img/shop/nike-classic-polo-t-shirt.jpg",
+        "price": 23
+    },
+    {
+        "id": "8",
+        "title": "Nike Winter Jacket in Blue",
+        "imageUrl": "img/shop/nike-winter-jacket-in-blue.jpg",
+        "price": 85
+    },
+    {
+        "id": "9",
+        "title": "Puma Basket Shoes",
+        "imageUrl": "img/shop/puma-basket-shoes.jpg",
+        "price": 140
+    },
+    {
+        "id": "10",
+        "title": "Puma Essential Tank Top",
+        "imageUrl": "img/shop/puma-essentialtank-top.jpg",
+        "price": 43
+    },
+    {
+        "id": "11",
+        "title": "Puma Fitness Shoes in Grey",
+        "imageUrl": "img/shop/puma-fitness-shoes-in-grey.jpg",
+        "price": 85
+    },
+    {
+        "id": "12",
+        "title": "Puma Sports Bra",
+        "imageUrl": "img/shop/puma-sports-bra.jpg",
+        "price": 12
+    },
+    {
+        "id": "13",
+        "title": "Puma Textile Running Shoes",
+        "imageUrl": "img/shop/puma-textile-running-shoes.jpg",
+        "price": 62
+    },
+    {
+        "id": "14",
+        "title": "Quicksilver Tech T-Shirt",
+        "imageUrl": "img/shop/quicksilver-tech-t-shirt.jpg",
+        "price": 21
+    },
+    {
+        "id": "15",
+        "title": "Quicksilver Workout Top",
+        "imageUrl": "img/shop/quicksilver-workout-top.jpg",
+        "price": 12
+    },
+    {
+        "id": "16",
+        "title": "Reebok Fitness Jacket",
+        "imageUrl": "img/shop/reebok-fitness-jacket.jpg",
+        "price": 52
+    },
+    {
+        "id": "17",
+        "title": "Reebok Long Sleeve Top",
+        "imageUrl": "img/shop/reebok-long-sleeve-top.jpg",
+        "price": 18
+    },
+    {
+        "id": "18",
+        "title": "Reebok Red Duster",
+        "imageUrl": "img/shop/reebok-red-duster.jpg",
+        "price": 60
+    },
+    {
+        "id": "19",
+        "title": "Reebok Workout Shorts",
+        "imageUrl": "img/shop/reebok-workout-shorts.jpg",
+        "price": 25
     }
 ]`;
 
